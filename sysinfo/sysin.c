eee64885691bc9bfe541fa2bd75bfb00b25e37c0
gtkn <admin@abby0666.xyz>
2024-07-25 05:14:31 -0700

    f

--- sysinfo/sysin.c
+++ sysinfo/sysin.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void run_command(const char *command) {
-    FILE *fp;
-    char path[1035];
-    fp = _popen(command, "r");
-    if (fp == NULL) {
-        printf("Failed to run command: %s\n", command);
-        exit(1);
-    }
-    while (fgets(path, sizeof(path)-1, fp) != NULL) {
-        printf("%s", path);
-    }
-    _pclose(fp);
+void run_command(const char* command) {
+	FILE* fp;
+	char path[4096];
+	int buffer_size = sizeof(path);
+
+	fp = _popen(command, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Failed to run command: %s\n", command);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(path, buffer_size - 1, fp) != NULL) {
+		if (strlen(path) >= buffer_size - 1) {
+			fprintf(stderr, "Output exceeds buffer size for command: %s\n", command);
+			_pclose(fp);
+			exit(EXIT_FAILURE);
+		}
+		printf("%s", path);
+	}
+
+	if (_pclose(fp) == -1) {
+		fprintf(stderr, "Failed to close command stream: %s\n", command);
+		exit(EXIT_FAILURE);
+	}
 }
 
 int main() {
-    run_command("systeminfo | findstr /B /C:\"OS Name\" /C:\"OS Version\"");
-    run_command("wmic qfe get Caption,Description,HotFixID,InstalledOn");
-    run_command("wmic os get osarchitecture || echo %PROCESSOR_ARCHITECTURE%");
+	run_command("systeminfo | findstr /B /C:\"OS Name\" /C:\"OS Version\"");
+	run_command("wmic qfe get Caption,Description,HotFixID,InstalledOn");
+	run_command("wmic os get osarchitecture || echo %PROCESSOR_ARCHITECTURE%");
 
-    return 0;
+	return 0;
 }
-
